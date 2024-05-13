50d6ae98fac7fb8e343d26b2619596c376f28e30
Tyler Lam <tylerlam@Tylers-MacBook-Pro.local>
2024-05-13 16:43:36 -0400

    a1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ import TOC from "./TOC";
 export default function Labs() {
     return (
         <div>
+            <h1>Tyler Lam WEBDEV SU1 2024</h1>
             <h1>Labs</h1>
             <TOC />
             <Routes>
